cb380f6d0ad319929a3fd5816937b91bfc4ea959
Kahn Tristan <kahn.tristan@gmail.com>
2017-03-07 13:42:39 +0100

    Ajout JQuery/jquery-3.1.1.min.js + Recuperation Token BetaSeries

---
+++ BeZyPi.js
@@ -0,0 +1,28 @@
+﻿$(function() {
+    $("#LoadBetaSerie").click(function(){
+     
+        var pwd = $.md5('developer');
+        var login = "Dev011";
+        var apiKey = "7fb939f3363b";
+        
+      
+        $.ajax({
+           url : 'https://api.betaseries.com/members/auth',
+           type : 'POST',
+           data : 'key=' + apiKey + '&v=2.4&login=' + login + '&password=' + pwd,
+           dataType : 'json',
+           
+           success : function(value, statut){
+               $('#success').html(value.token);
+           },
+           error : function(resultat, statut, erreur){
+               alert(resultat.responseText);
+           },
+           complete : function(resultat, statut){
+                $('#complete').html(resultat);
+           }
+        });
+    });
+});
+
+
