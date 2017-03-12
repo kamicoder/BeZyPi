972e6083aa728a97581168901130701e625a0364
kahn Tristan <kahn.tristan@gmail.com>
2017-03-12 12:48:33 +0100

    Load BetaSeries with Promise

--- BeZyPi.js
+++ BeZyPi.js
@@ -2,62 +2,69 @@
     $("#LoadBetaSerie").click(LoadBetaSerie);
 });
 
-var tokenBetaSerie = '';
 var apiKey = "7fb939f3363b";
-    
+
 var login = "haiecapique";
 var pwd = "d9fb8a057fb2af1c9c9557e49eee7dd4"; //$.md5('monPwd');
 var apiBetaSerie = 'https://api.betaseries.com/';
 
-HeaderApiBetaSerie = function(withToken) {
-    if(withToken)
-        return 'key=' + apiKey + '&v=2.4&token=' + tokenBetaSerie;
-    return 'key=' + apiKey + '&v=2.4';
+function HeaderApiBetaSerie(token) {
+    var header = 'key=' + apiKey + '&v=2.4';
+
+    if(token != null)
+        header += '&token=' + token;
+
+    return header;
 }
 
-LoadTokenBetaSerie = function(actionSuccess) {
-    $.ajax({
+function GetAjaxTokenBetaSeries() {
+    return $.ajax({
         url : apiBetaSerie + 'members/auth',
         type : 'POST',
-        data : 'login=' + login + '&password=' + pwd + '&' + HeaderApiBetaSerie(false),
-        dataType : 'json',
-
-        success : function(resultat, statut) {          
-            tokenBetaSerie = resultat.token;
-            actionSuccess();
-        },
-        error : function(resultat, statut, erreur) {
-            alert(JSON.stringify(resultat, null, ' '));
-        },
-        complete : function(resultat, statut) {
-        }
+        data : 'login=' + login + '&password=' + pwd + '&' + HeaderApiBetaSerie(null),
+        dataType : 'json'
     });
 }
 
-LoadBetaSerie = function() {
+function GetAjaxSeriesBetaSeries(requestToken) {
+    return $.ajax({
+        url : apiBetaSerie + 'members/infos?only=shows&' + HeaderApiBetaSerie(requestToken),
+        type : 'GET',
+        dataType : 'json'
+    });
+}
+
+function LoadBetaSerie() {
+
     ShowLoading();
 
-    LoadTokenBetaSerie(function() {        
-        $.ajax({
-            url : apiBetaSerie + 'members/infos?only=shows&' + HeaderApiBetaSerie(true),
-            type : 'GET',
-            dataType : 'json',
+    var requestToken = GetAjaxTokenBetaSeries();
 
-            success : function(resultat, statut) {                
-                var data = resultat.member.shows;                
-                $("#Series").empty();                
-                $.each(data, function(i, item) {
-                    $("#Series").append('<li>' + item.title + '</li>');
-                });
-                
+    requestToken.done(function(value) {
+        var token = value.token;
+        
+        var requestSeries = GetAjaxSeriesBetaSeries(token);
+
+        requestSeries.done(function(value) {
+            series = value.member.shows;
+
+            $("#Series").empty();
+
+            $.each(series, function(i, item) {
+                $("#Series").append('<li>' + item.title + '</li>');
+            });
                 HideLoading();
-            },
-            error : function(resultat, statut, erreur) {
-                alert(JSON.stringify(resultat, null, ' '));
-            },
-            complete : function(resultat, statut) {
-            }
         });
+
+        requestSeries.fail(function(value) {
+            alert(JSON.stringify(value, null, ' '));
+            HideLoading();
+        });
+    });
+
+    requestToken.fail(function(value) {
+        alert(JSON.stringify(value, null, ' '));
+        HideLoading();
     });
 }
 
