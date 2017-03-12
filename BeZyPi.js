5541731a0315872e8f2272d1280dcb704bca4d86
kahn Tristan <kahn.tristan@gmail.com>
2017-03-12 10:20:50 +0100

    Add loading window

--- BeZyPi.js
+++ BeZyPi.js
@@ -3,7 +3,8 @@
 });
 
 LoadBetaSerie = function() {
-    cursor_wait();
+
+    ShowLoading();
     
     var pwd = $.md5('developer');
     var login = "Dev011";
@@ -15,35 +16,47 @@ LoadBetaSerie = function() {
         data : 'key=' + apiKey + '&v=2.4&login=' + login + '&password=' + pwd,
         dataType : 'json',
 
-        success : function(value, statut){
+        success : function(value, statut) {
             $('#success').html(value.token);
         },
-        error : function(resultat, statut, erreur){
+        error : function(resultat, statut, erreur) {
             alert(resultat.responseText);
         },
-        complete : function(resultat, statut){
-            remove_cursor_wait();
+        complete : function(resultat, statut) {
+            HideLoading();
         }
     });
 }
 
-cursor_wait = function() {
-    // switch to cursor wait for the current element over
-    var elements = $(':hover');
-    if (elements.length) {
-        // get the last element which is the one on top
-        elements.last().addClass('cursor-wait');
-    }
-    // use .off() and a unique event name to avoid duplicates
-    $('html').
-        off('mouseover.cursorwait').
-        on('mouseover.cursorwait', function(e) {
-            // switch to cursor wait for all elements you'll be over
-            $(e.target).addClass('cursor-wait');
-        });
-}
+ShowLoading = function(opts) {
+    var options = {
+        imgPath: 'loading.svg',
+        imgStyle: {
+            width: 'auto',
+            textAlign: 'center',
+            marginTop: '20%'
+        },
+        text: 'Chargement...',
+        style: {
+            position: 'fixed',
+            width: '100%',
+            height: '100%',
+            background: 'rgba(0, 0, 0, .8)',
+            left: 0,
+            top: 0,
+            zIndex: 10000
+        }
+    };
 
-remove_cursor_wait = function() {
-    $('html').off('mouseover.cursorwait'); // remove event handler
-    $('.cursor-wait').removeClass('cursor-wait'); // get back to default
-}
+    $.extend(options, opts);
+
+    var img = $('<div><img src="' + options.imgPath + '"><div>' + options.text + '</div></div>');
+    var block = $('<div id="loading_block"></div>');
+
+    block.css(options.style).appendTo('body');
+    img.css(options.imgStyle).appendTo(block);
+};
+
+HideLoading = function() {
+    $('div#loading_block').remove();
+};
